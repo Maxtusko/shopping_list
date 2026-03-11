c0ef2d1935bcfdf0dbd86062d94e2c33e87b0922
matus.nazarej <matus.nazarej@accenture.com>
2026-03-11 15:51:27 +0100

    Shopping List

---
+++ shopping_list.py
@@ -0,0 +1,50 @@
+print("Hi there, welcome to the shopping list")
+
+shopping_list = {}
+status = input("Type 'NEW' for new Shopping List or Type 'C' to add items to the existing Shopping List: ").strip().lower()
+
+while True:
+    item_to_buy = input("What are you planning to buy? Enter: ")
+    if item_to_buy.strip().lower() == "done":
+        break
+
+    if item_to_buy in shopping_list:
+        print(f"\nYou already have {item_to_buy.upper()} on the list. Actual amount: {shopping_list[item_to_buy.lower()]}\n")
+        adjust = input("Adjust the quantity? (yes/no): ").strip().lower()
+
+        if adjust in ("yes", "y"):
+            quantity = input("Enter new quantity: ")
+            shopping_list[item_to_buy] = quantity.upper()
+
+    else:
+        quantity = input("How many items do you want? Qs: ")
+        shopping_list[item_to_buy.lower()] = quantity.upper()
+
+if status == "new":
+    mode = "w"
+elif status == "c":
+    mode = "a"
+else:
+    print("Invalid option chosen. New Shopping List will be created.")
+    mode = "w"
+
+
+with open("Data/shopping_list.txt", mode) as file:
+    if mode == "w":
+        file.write("SHOPPING LIST:\n\n")
+    for i, (product, quantity) in enumerate(shopping_list.items(), start=1):
+        print(f"{i}. {product}: {quantity}")
+        file.write(f"{i}. {product}: {quantity}\n")
+
+import pywhatkit as kit
+try:
+    with open("Data/shopping_list.txt", "r") as file:
+        message = file.read()
+
+    kit.sendwhatmsg_instantly("+421951717455", message)
+except FileNotFoundError as error:
+    print("Sorry, something went wrong. Try again later.")
+
+
+
+
